8aba1cf316663d3b64b0f527df8ab7aabc962b66
Agatha North <rouborn@gmail.com>
2020-02-21 11:54:32 -0800

    npm thing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import $ from 'jquery';
 
+
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
 import shoppingList from './shopping-list';
 
+
+
 const main = function () {
+  api.getItems()
+  .then(res => console.log(res));
+
+console.log(api.BASE_URL);
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
